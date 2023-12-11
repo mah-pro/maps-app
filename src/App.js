9acf8f22a741b7eeef0c561b8a0054d9523eb1a9
mah-pro <142360247+mah-pro@users.noreply.github.com>
2023-12-11 14:19:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
-const apiKey = 'AIzaSyA98gZqNk5ggt80pt0rJrWFMYrZfB1y8d0';
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-const Map = withScriptjs(
-  withGoogleMap(() => (
-    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 37.7749, lng: -122.4194 }}>
-      <Marker position={{ lat: 37.7749, lng: -122.4194 }} />
-    </GoogleMap>
-  ))
-);
-
-function App() {
+export default function Map(){
+  const defaultProps = {
+    center: {
+         
+      lat: 14.69789671611395, 
+      lng:-17.45558295624316
+    },
+    zoom: 11
+  };
 
   return (
-    <Container className="mt-5">
-      <Row>
-        <Col className="text-center">
-          <h1>Ma Page de Destination</h1>
-          <p>Une application incroyable avec Google Maps!</p>
-        </Col>
-      </Row>
-      <Row className="justify-content-center mt-4">
-        <Col xs={12} md={8}>
-          <Map
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`}
-            loadingElement={<div style={{ height: '100%' }} />}
-            containerElement={<div style={{ height: '400px' }} />}
-            mapElement={<div style={{ height: '100%' }} />}
-          />
-        </Col>
-      </Row>
-    </Container>
+    // Important! Always set the container height explicitly
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
   );
 }
-
-export default App;
